c9899b9e66b4f4c7545b52af9ca91a6aee215ece
Amaury <amaury@56k.be>
2016-09-03 16:14:25 +0200

    First try for the dashboard page
    Need to add localisation var in the page

---
+++ core/cron/gym.cron.php
@@ -0,0 +1,102 @@
+<?php
+	
+// -----------------------------------------------------------------------------------------------------------
+// Gym datas 
+// Total gym
+// Gym / team 
+// Average level / team 
+// -----------------------------------------------------------------------------------------------------------
+
+
+$gym['timestamp'] 	=  $timestamp; 
+
+$req 	= "SELECT count( DISTINCT(gym_id) ) as total FROM gym";
+$result = $mysqli->query($req); 
+$data 	= $result->fetch_object();
+
+$gym['total'] 		= $data->total; 
+
+
+// Mystic
+
+$req 	= "SELECT count( DISTINCT(gym_id) ) as total FROM gym WHERE team_id = '1'  "; 
+$result = $mysqli->query($req); 
+$data 	= $result->fetch_object();
+
+$gym['team']['mystic']['gym_owned'] 		= $data->total; 
+
+
+
+$req 	= "SELECT gym_points FROM gym WHERE team_id = '1'  "; 
+$result = $mysqli->query($req); 
+
+$total_points=0; 
+
+while($data = $result->fetch_object()){
+
+	$total_points = $total_points + $data->gym_points; 
+	
+}
+
+$gym['team']['mystic']['average'] 			= round($total_points / $gym['team']['mystic']['gym_owned']);
+
+
+
+// Valor
+
+$req 	= "SELECT count( DISTINCT(gym_id) ) as total FROM gym WHERE team_id = '2'  "; 
+$result = $mysqli->query($req); 
+$data 	= $result->fetch_object();
+
+$gym['team']['valor']['gym_owned'] 			= $data->total; 
+
+
+
+$req 	= "SELECT gym_points FROM gym WHERE team_id = '2'  "; 
+$result = $mysqli->query($req); 
+
+$total_points=0; 
+
+while($data = $result->fetch_object()){
+
+	$total_points = $total_points + $data->gym_points; 
+	
+}
+
+$gym['team']['valor']['average'] 			= round($total_points / $gym['team']['valor']['gym_owned']);
+
+
+// Instinct
+
+$req = "SELECT count( DISTINCT(gym_id) ) as total FROM gym WHERE team_id = '3'  "; 
+$result = $mysqli->query($req); 
+$data 	= $result->fetch_object();
+
+$gym['team']['instinct']['gym_owned'] 		= $data->total; 
+
+
+
+$req 	= "SELECT gym_points FROM gym WHERE team_id = '3'  "; 
+$result = $mysqli->query($req); 
+
+$total_points=0; 
+
+while($data = $result->fetch_object()){
+
+	$total_points = $total_points + $data->gym_points; 
+	
+}
+
+$gym['team']['instinct']['average'] 		= round($total_points / $gym['team']['instinct']['gym_owned']);
+
+
+// Add the datas in file
+
+$gymsdatas[] 	= $gym; 
+$json 			= json_encode($gymsdatas); 
+
+file_put_contents($gym_file, $json);
+
+
+
+?>
