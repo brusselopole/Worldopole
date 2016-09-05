56361a2416b54d5c4e4e614247ec3b0cd87fcea2
Markus Hilger <obihoernchende@gmail.com>
2016-09-05 14:19:43 +0200

    Hide JS if there is no trainer level info available

--- core/js/gym.graph.js.php
+++ core/js/gym.graph.js.php
@@ -50,86 +50,87 @@ for ($teamid = 1; $teamid <= 3; $teamid++) {
 }
 $mysqli->close();
 
-?>
-
-// Global Options
-// --------------
-
-Chart.defaults.global.legend.display = false;
-
-// Trainer level
-// -------------
-
 // Hide trainer level graph if there is no result (gym-info not active in PokemonGo-Map)
-<?php if (!$trainer_lvl) { ?> document.getElementById('trainer_lvl_graph').style.display = 'none'; <?php } ?>
+// and only show javascript if it's set
+if (!$trainer_lvl) { ?>
+	document.getElementById('trainer_lvl_graph').style.display = 'none';
+<?php } else { ?>
+	// Global Options
+	// --------------
 
-var trainer_lvl = $("#trainer_lvl");
+	Chart.defaults.global.legend.display = false;
 
-var data_trainer_lvl = {
-    labels: [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40],
-    datasets: [
-        {
-            label: "Mystic Trainer Level count",
-            backgroundColor: "rgba(59,129,255,0.4)",
-            borderColor: "rgba(59,129,255,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(59,129,255,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(59,129,255,1)",
-            pointHoverBorderColor: "rgba(59,129,255,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [<?= implode(',', $trainer_lvl[1] )?>],
-        },
-        {
-            label: "Valor Trainer Level count",
-            backgroundColor: "rgba(247,10,20,0.4)",
-            borderColor: "rgba(247,10,20,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(247,10,20,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(247,10,20,1)",
-            pointHoverBorderColor: "rgba(247,10,20,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [<?= implode(',', $trainer_lvl[2] )?>],
-        },
-        {
-            label: "Instinct Trainer Level count",
-            backgroundColor: "rgba(248,153,0,0.4)",
-            borderColor: "rgba(248,153,0,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(248,153,0,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(248,153,0,1)",
-            pointHoverBorderColor: "rgba(248,153,0,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [<?= implode(',', $trainer_lvl[3] )?>],
-        }
-    ]
-};
+	// Trainer level
+	// -------------
 
-var myLineChart = new Chart(trainer_lvl, {
-    type: 'bar',
-    data: data_trainer_lvl,
-    options : ''
-});
+	var trainer_lvl = $("#trainer_lvl");
+
+	var data_trainer_lvl = {
+	    labels: [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40],
+	    datasets: [
+		{
+		    label: "Mystic Trainer Level count",
+		    backgroundColor: "rgba(59,129,255,0.4)",
+		    borderColor: "rgba(59,129,255,1)",
+		    borderCapStyle: 'butt',
+		    borderDash: [],
+		    borderDashOffset: 0.0,
+		    borderJoinStyle: 'miter',
+		    pointBorderColor: "rgba(59,129,255,1)",
+		    pointBackgroundColor: "#fff",
+		    pointBorderWidth: 1,
+		    pointHoverRadius: 5,
+		    pointHoverBackgroundColor: "rgba(59,129,255,1)",
+		    pointHoverBorderColor: "rgba(59,129,255,1)",
+		    pointHoverBorderWidth: 2,
+		    pointRadius: 1,
+		    pointHitRadius: 10,
+		    data: [<?= implode(',', $trainer_lvl[1] )?>],
+		},
+		{
+		    label: "Valor Trainer Level count",
+		    backgroundColor: "rgba(247,10,20,0.4)",
+		    borderColor: "rgba(247,10,20,1)",
+		    borderCapStyle: 'butt',
+		    borderDash: [],
+		    borderDashOffset: 0.0,
+		    borderJoinStyle: 'miter',
+		    pointBorderColor: "rgba(247,10,20,1)",
+		    pointBackgroundColor: "#fff",
+		    pointBorderWidth: 1,
+		    pointHoverRadius: 5,
+		    pointHoverBackgroundColor: "rgba(247,10,20,1)",
+		    pointHoverBorderColor: "rgba(247,10,20,1)",
+		    pointHoverBorderWidth: 2,
+		    pointRadius: 1,
+		    pointHitRadius: 10,
+		    data: [<?= implode(',', $trainer_lvl[2] )?>],
+		},
+		{
+		    label: "Instinct Trainer Level count",
+		    backgroundColor: "rgba(248,153,0,0.4)",
+		    borderColor: "rgba(248,153,0,1)",
+		    borderCapStyle: 'butt',
+		    borderDash: [],
+		    borderDashOffset: 0.0,
+		    borderJoinStyle: 'miter',
+		    pointBorderColor: "rgba(248,153,0,1)",
+		    pointBackgroundColor: "#fff",
+		    pointBorderWidth: 1,
+		    pointHoverRadius: 5,
+		    pointHoverBackgroundColor: "rgba(248,153,0,1)",
+		    pointHoverBorderColor: "rgba(248,153,0,1)",
+		    pointHoverBorderWidth: 2,
+		    pointRadius: 1,
+		    pointHitRadius: 10,
+		    data: [<?= implode(',', $trainer_lvl[3] )?>],
+		}
+	    ]
+	};
+
+	var myLineChart = new Chart(trainer_lvl, {
+	    type: 'bar',
+	    data: data_trainer_lvl,
+	    options : ''
+	});
+<?php } ?>
