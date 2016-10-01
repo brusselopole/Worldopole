152bbfdb9d8534e22c4e1d6ae05d4acd3ae35c4b
Markus Hilger <obihoernchende@gmail.com>
2016-10-01 04:28:26 +0200

    Format home.script.js

--- core/js/home.script.js
+++ core/js/home.script.js
@@ -14,66 +14,62 @@ function updateCounter(new_value, classname){
 
 
 
-
-
-
 (function cron() {
-   
-    $.ajax({
-        url: 'core/process/aru.php?type=home_update',
-        success: function(data) {           
-        	        	
-        	var pokemon = data[0]; 
-        	var lure	= data[1];  
-        	
-        	var red 	= data[2];
-        	var blue 	= data[3];
-        	var yellow 	= data[4];
-        	var neutral = data[5]; 
-        	
-        	updateCounter(pokemon,'.total-pkm-js');
-        	updateCounter(lure,'.total-lure-js');
-        	updateCounter(red,'.total-valor-js');
-        	updateCounter(blue,'.total-mystic-js');
-        	updateCounter(yellow,'.total-instinct-js');
-        	updateCounter(neutral,'.total-new-js');
-        	
-        },
-        complete: function() {
-            // Schedule the next request when the current one's complete
-            setTimeout(cron, 5000);
-        }
-    });
+ 
+	$.ajax({
+		url: 'core/process/aru.php?type=home_update',
+		success: function(data) {
+					
+			var pokemon = data[0]; 
+			var lure	= data[1];
+			
+			var red 	= data[2];
+			var blue 	= data[3];
+			var yellow 	= data[4];
+			var neutral = data[5]; 
+			
+			updateCounter(pokemon,'.total-pkm-js');
+			updateCounter(lure,'.total-lure-js');
+			updateCounter(red,'.total-valor-js');
+			updateCounter(blue,'.total-mystic-js');
+			updateCounter(yellow,'.total-instinct-js');
+			updateCounter(neutral,'.total-new-js');
+			
+		},
+		complete: function() {
+			// Schedule the next request when the current one's complete
+			setTimeout(cron, 5000);
+		}
+	});
 })();
 
 
 
-
 (function spawn() {
-   
-   var last_id = $('.last-mon-js div:first-child').attr('pokeid'); 
-   //console.log(last_id); 
-   
-    $.ajax({
-        url: 'core/process/aru.php?type=spawnlist_update&last_id='+last_id,
-        success: function(data) {           
-        	
-        	if(data != ''){
+ 
+	var last_id = $('.last-mon-js div:first-child').attr('pokeid'); 
+	//console.log(last_id); 
+	 
+	$.ajax({
+		url: 'core/process/aru.php?type=spawnlist_update&last_id='+last_id,
+		success: function(data) {
+			
+			if(data != ''){
 
-	        	//console.log(data);
-	        	$('.last-mon-js').prepend(data);
-	        	
-	        	$('.last-mon-js div:last-child').fadeOut();
-	        	$('.last-mon-js div:first-child').fadeIn();
+				//console.log(data);
+				$('.last-mon-js').prepend(data);
 				
-				$('.last-mon-js div:last-child').remove();
-        	}
-        	
-        	
-        },
-        complete: function() {
-            // Schedule the next request when the current one's complete
-            setTimeout(spawn, 5000);
-        }
-    });
+				$('.last-mon-js div:last-child').fadeOut();
+				$('.last-mon-js div:first-child').fadeIn();
+					
+					$('.last-mon-js div:last-child').remove();
+			}
+			
+			
+		},
+		complete: function() {
+			// Schedule the next request when the current one's complete
+			setTimeout(spawn, 5000);
+		}
+	});
 })();
