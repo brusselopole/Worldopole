67a469d96aa84279a68e9ceaef1e9d5c4981af02
Markus Hilger <obihoernchende@gmail.com>
2016-10-01 02:50:32 +0200

    Format 404.page.php

--- pages/404.page.php
+++ pages/404.page.php
@@ -1,15 +1,15 @@
 <header id="single-header">
-<div class="row">
-	<div class="col-md-12 text-center">
+	<div class="row">
+		<div class="col-md-12 text-center">
 
-		<center>
-			<img src="core/img/compass.png" class="img-responsive" alt="Not found" width=150 />
-		</center>
+			<center>
+				<img src="core/img/compass.png" class="img-responsive" alt="Not found" width=150 />
+			</center>
 
-		<h1>
-			<?= $locales->ERROR->$lang ?>
-		</h1>
+			<h1>
+				<?= $locales->ERROR->$lang ?>
+			</h1>
 
+		</div>
 	</div>
-</div>
 </header>
