2d2361dea4aaef616556e3e437b3160f8546047d
Markus Hilger <obihoernchende@gmail.com>
2016-10-01 04:33:25 +0200

    Format pokemon.maps.js.php

--- core/js/pokemon.maps.js.php
+++ core/js/pokemon.maps.js.php
@@ -19,12 +19,10 @@ include_once('../../config.php');
 $mysqli = new mysqli(SYS_DB_HOST, SYS_DB_USER, SYS_DB_PSWD, SYS_DB_NAME, SYS_DB_PORT);
 if($mysqli->connect_error != ''){exit('Error MySQL Connect');}
 
-# Heatmap datas  
+# Heatmap datas
 $pokemon_id = mysqli_real_escape_string($mysqli,$_GET['id']);
 
-
 ?>
-
 
 
 var map, heatmap;
@@ -34,41 +32,41 @@ function initMap() {
 	$.getJSON( "core/json/variables.json", function( jsondata ) {
 				
 		var variables = jsondata; 
-  				  				  		
-  		var lattitude = Number(variables['system']['map_center_lat']); 
-  		var longitude = Number(variables['system']['map_center_long']);
-  		var zomm_level = Number(variables['system']['zomm_level']);
-  		
-	  		
+ 		 		
+		var lattitude = Number(variables['system']['map_center_lat']); 
+		var longitude = Number(variables['system']['map_center_long']);
+		var zomm_level = Number(variables['system']['zomm_level']);
+		
+			
 		map = new google.maps.Map(document.getElementById('map'), {
 			center: {lat: lattitude, lng: longitude},
-		    zoom: zomm_level,
-		    zoomControl: true,
+			zoom: zomm_level,
+			zoomControl: true,
 			scaleControl: false,
 			scrollwheel: true,
 			disableDoubleClickZoom: false,
 		});
 		
 		heatmap = new google.maps.visualization.HeatmapLayer({
-		  data: getPoints(),
-		  map: map
+			data: getPoints(),
+			map: map
 		});
 		
 		var gradient = [
-		  'rgba(0, 255, 255, 0)',
-		  'rgba(0, 255, 255, 1)',
-		  'rgba(0, 191, 255, 1)',
-		  'rgba(0, 127, 255, 1)',
-		  'rgba(0, 63, 255, 1)',
-		  'rgba(0, 0, 255, 1)',
-		  'rgba(0, 0, 223, 1)',
-		  'rgba(0, 0, 191, 1)',
-		  'rgba(0, 0, 159, 1)',
-		  'rgba(0, 0, 127, 1)',
-		  'rgba(63, 0, 91, 1)',
-		  'rgba(127, 0, 63, 1)',
-		  'rgba(191, 0, 31, 1)',
-		  'rgba(255, 0, 0, 1)'
+			'rgba(0, 255, 255, 0)',
+			'rgba(0, 255, 255, 1)',
+			'rgba(0, 191, 255, 1)',
+			'rgba(0, 127, 255, 1)',
+			'rgba(0, 63, 255, 1)',
+			'rgba(0, 0, 255, 1)',
+			'rgba(0, 0, 223, 1)',
+			'rgba(0, 0, 191, 1)',
+			'rgba(0, 0, 159, 1)',
+			'rgba(0, 0, 127, 1)',
+			'rgba(63, 0, 91, 1)',
+			'rgba(127, 0, 63, 1)',
+			'rgba(191, 0, 31, 1)',
+			'rgba(255, 0, 0, 1)'
 		];
 		
 		heatmap.set('gradient', gradient);
@@ -84,26 +82,24 @@ function initMap() {
 		
 		function getPoints() {
 		return [
-		  
-		  <?php
-		  
-		  // As the map is rendering by the client, we do recommand to keep a limit on your request. 
-		  // 10k is already alotof datas ;) 
-		  
-		  $req 	= "SELECT * FROM pokemon WHERE pokemon_id = '".$pokemon_id."' ORDER BY disappear_time DESC LIMIT 0,10000";
-		  $result = $mysqli->query($req);   
-		  	      
-		  while($data = $result->fetch_object()){
-			  
-		      		        
-		  ?>
-		  
-		   new google.maps.LatLng(<?= $data->latitude ?>, <?= $data->longitude ?>),
-		  
-		  <?php }?>
-		  
-		 
+		
+			<?php
+			
+			// As the map is rendering by the client, we do recommand to keep a limit on your request. 
+			// 10k is already alotof datas ;) 
+			
+			$req 	= "SELECT * FROM pokemon WHERE pokemon_id = '".$pokemon_id."' ORDER BY disappear_time DESC LIMIT 0,10000";
+			$result = $mysqli->query($req);
+			
+			while($data = $result->fetch_object()){
+			 
+			
+			?>
+			
+			 new google.maps.LatLng(<?= $data->latitude ?>, <?= $data->longitude ?>),
+			
+			<?php }?>
+		
 		];
 		
-	
 }
