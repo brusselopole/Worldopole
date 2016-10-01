485bb71f76bcb0089234c25979c9476b0081c5c3
Markus Hilger <obihoernchende@gmail.com>
2016-10-01 03:20:53 +0200

    Format pokedex.page.php

--- pages/pokedex.page.php
+++ pages/pokedex.page.php
@@ -1,12 +1,12 @@
 <header id="single-header">
-<div class="row">
-	<div class="col-md-12 text-center">
-		<h1>
-			<?= $locales->POKEDEX_TITLE->$lang ?>
-		</h1>
-		
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<h1>
+				<?= $locales->POKEDEX_TITLE->$lang ?>
+			</h1>
+			
+		</div>
 	</div>
-</div>
 </header>
 
 <div class="row">
@@ -14,7 +14,7 @@
 	<div class="col-md-12">
 	
 		<div class="search form-group">
-	    	<input type="search" class="form-control" placeholder="search here" required>
+			<input type="search" class="form-control" placeholder="search here" required>
 		</div>
 	
 	</div>
@@ -28,7 +28,7 @@
 			
 				<a href="<?= $pokemon->permalink ?>"><img src="<?= $pokemon->img ?>" alt="<?= $pokemon->name ?>" class="img-responsive <?php if($pokemon->spawn == 0){ echo 'unseen'; } ?> "></a>
 				<p class="pkmn-name"><a href="<?= $pokemon->permalink ?>"><?= $pokemon->name ?></a></p>
-				<p><?php  if($pokemon->spawn ==0){ echo $locales->UNSEEN->$lang ; }else{ echo $pokemon->spawn. $locales->SEEN->$lang ; } ?> </p>
+				<p><?php if($pokemon->spawn ==0){ echo $locales->UNSEEN->$lang ; }else{ echo $pokemon->spawn. $locales->SEEN->$lang ; } ?> </p>
 			
 			</div>
 			
