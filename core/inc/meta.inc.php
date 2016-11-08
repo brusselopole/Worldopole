52f0143fb698b218d45df8b001e529686c513d25
Markus Hilger <obihoernchende@gmail.com>
2016-11-08 06:08:08 +0100

    Add metadata for trainer page

--- core/inc/meta.inc.php
+++ core/inc/meta.inc.php
@@ -125,8 +125,32 @@
 		<meta name="twitter:image" content="<?= HOST_URL ?>core/img/logo.jpg" />
 
 		<?php
-		break; 
-		
+		break;	
+
+		case 'trainer':
+		?>
+
+		<title><?= $config->infos->site_name ?> | <?= $locales->TRAINERS_METATITLE->$lang ?></title>
+
+		<link rel="canonical" href="<?= HOST_URL ?>/trainer" />
+		<base href="<?= HOST_URL ?>">
+
+		<meta property="og:locale" content="en_US" />
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content="<?= $config->infos->site_name ?> | <?= $locales->TRAINERS_METATITLE->$lang ?>" />
+		<meta property="og:description" content="<?= $locales->TRAINERS_METADESC->$lang ?>" />
+		<meta property="og:url" content="<?= HOST_URL ?>/trainer" />
+		<meta property="og:site_name" content="<?= $config->infos->site_name ?>" />
+		<meta property="og:image" content="<?= HOST_URL ?>core/img/logo.jpg" />
+
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:description" content="<?= $locales->TRAINERS_METADESC->$lang ?>" />
+		<meta name="twitter:title" content="<?= $config->infos->site_name ?> | <?= $locales->TRAINERS_METATITLE->$lang ?>" />
+		<meta name="twitter:image" content="<?= HOST_URL ?>core/img/logo.jpg" />
+
+		<?php
+		break;
+
 		case 'dashboard':
 		?>
 		
