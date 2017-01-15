523d102e9d902ff03c0c6b02a060aec6db1b905a
klatterhans <intellhosp@gmail.com>
2017-01-15 16:46:01 +0100

    Update trainer.content.js

--- core/js/trainer.content.js
+++ core/js/trainer.content.js
@@ -114,7 +114,6 @@ $(function () {
 					progressBar.append($('<div>',{title: 'IV Stamina :'+pokemon.iv_stamina, class: 'progress-bar progress-bar-success' ,role : 'progressbar', 'aria-valuenow' :pokemon.iv_stamina, 'aria-valuemin' : 0, 'aria-valuemax' : 45}).css('width',((100/45)*pokemon.iv_stamina ) + '%'))
 					progressBar.append($('<div>',{title: 'IV Attack :'+pokemon.iv_attack, class: 'progress-bar progress-bar-danger' ,role : 'progressbar', 'aria-valuenow' : pokemon.iv_attack, 'ria-valuemin' : 0, 'aria-valuemax' : 45}).css('width',((100/45)*pokemon.iv_attack ) + '%'))
 					progressBar.append($('<div>',{title: 'IV Defense :'+pokemon.iv_defense, class: 'progress-bar progress-bar-info' ,role : 'progressbar', 'aria-valuenow': pokemon.iv_defense, 'aria-valuemin' : 0, 'aria-valuemax' : 45}).css('width',((100/45)*pokemon.iv_defense ) + '%'))
-                    
 				trainerPokemon.append(progressBar);
 				if (pokemon.last_scanned == 0) {
                       			trainerPokemon.append($('<p>',{class : 'pkmn-name',text: "Today"}));
