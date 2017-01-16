bc0572832145495fbbc34a35b12ec0f8c6e9f7f1
klatterhans <intellhosp@gmail.com>
2017-01-16 10:44:02 +0100

    Style fixing. Smaller fontsize and "===" to "==".

--- core/js/trainer.content.js
+++ core/js/trainer.content.js
@@ -115,14 +115,14 @@ $(function () {
 					progressBar.append($('<div>',{title: 'IV Attack :'+pokemon.iv_attack, class: 'progress-bar progress-bar-danger' ,role : 'progressbar', 'aria-valuenow' : pokemon.iv_attack, 'ria-valuemin' : 0, 'aria-valuemax' : 45}).css('width',((100/45)*pokemon.iv_attack ) + '%'))
 					progressBar.append($('<div>',{title: 'IV Defense :'+pokemon.iv_defense, class: 'progress-bar progress-bar-info' ,role : 'progressbar', 'aria-valuenow': pokemon.iv_defense, 'aria-valuemin' : 0, 'aria-valuemax' : 45}).css('width',((100/45)*pokemon.iv_defense ) + '%'))
 					trainerPokemon.append(progressBar);
-					if (pokemon.last_scanned === 0) {
-                      				trainerPokemon.append($('<p>',{class : 'pkmn-name',text: "Today"}));
+					if (pokemon.last_scanned == 0) {
+                      				trainerPokemon.append($('<small>',{class : 'pkmn-name',text: "Today"}));
                     			}
-                    			else if (pokemon.last_scanned === 1) {
-                        			trainerPokemon.append($('<p>',{class : 'pkmn-name',text: pokemon.last_scanned + " Day"}));
+                    			else if (pokemon.last_scanned == 1) {
+                        			trainerPokemon.append($('<small>',{class : 'pkmn-name',text: pokemon.last_scanned + " Day"}));
                     			}
                     			else {
-                        			trainerPokemon.append($('<p>',{class : 'pkmn-name',text: pokemon.last_scanned + " Days"}));
+                        			trainerPokemon.append($('<small>',{class : 'pkmn-name',text: pokemon.last_scanned + " Days"}));
                    			}
 					trainersPokemonsContainer.append(trainerPokemon);
 				}
