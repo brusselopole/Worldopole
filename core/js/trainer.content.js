37cdc8c3d60f4e85e211ee31fd821cb5825e4e1b
Markus Hilger <obihoernchende@gmail.com>
2017-01-27 12:38:49 +0100

    Make images toggleable - New variable pokeimg_suffix to choose the image file

--- core/js/trainer.content.js
+++ core/js/trainer.content.js
@@ -1,4 +1,9 @@
 $(function () {
+	var pokeimg_suffix='';
+	$.getJSON( "core/json/variables.json", function( jsondata ) {
+		pokeimg_suffix=jsondata['system']['pokeimg_suffix'];
+	});
+
 	$('.trainerLoader').hide();
 	var page = 0;
 	var teamSelector=0; //0=all;1=Blue;2=Red;3=Yellow
@@ -108,7 +113,7 @@ $(function () {
 				for (var pokeIndex = 0; pokeIndex<trainer.pokemons.length; pokeIndex++) {
 					var pokemon = trainer.pokemons[pokeIndex];
 					var trainerPokemon = $('<div>',{id : 'trainerPokemon_'+pokemon.pokemon_uid, class: "col-md-1 col-xs-4 pokemon-single", style: "text-align: center" });
-					trainerPokemon.append($('<a>',{href : 'pokemon/'+pokemon.pokemon_id}).append($('<img />',{src : 'core/pokemons/'+pokemon.pokemon_id+'.png', 'class' : 'img-responsive '+(pokemon.gym_id===null?"unseen":"")})));
+					trainerPokemon.append($('<a>',{href : 'pokemon/'+pokemon.pokemon_id}).append($('<img />',{src : 'core/pokemons/'+pokemon.pokemon_id+pokeimg_suffix, 'class' : 'img-responsive '+(pokemon.gym_id===null?"unseen":"")})));
 					trainerPokemon.append($('<p>',{class : 'pkmn-name'}).append(pokemon.cp));
 					var progressBar = $('<div>',{class : 'progress'}).css({'height': '6px','margin-bottom': '0'});
 					progressBar.append($('<div>',{title: 'IV Stamina :'+pokemon.iv_stamina, class: 'progress-bar progress-bar-success' ,role : 'progressbar', 'aria-valuenow' :pokemon.iv_stamina, 'aria-valuemin' : 0, 'aria-valuemax' : 45}).css('width',((100/45)*pokemon.iv_stamina ) + '%'))
