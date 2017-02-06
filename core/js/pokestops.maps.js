24640d8e6ff891d38daec92b87508c62a61ccea5
klatterhans <intellhosp@gmail.com>
2017-02-06 16:16:29 +0100

    Add dark style to Pokestop map

--- core/js/pokestops.maps.js
+++ core/js/pokestops.maps.js
@@ -31,7 +31,33 @@ function initMap()
 					arr.push(JSON.parse(locations[i]));
 				}
 			
-			
+                var darkStyle = [{'featureType': 'all','elementType': 'labels.text.fill','stylers':
+                                       [{'saturation': 36},{'color': '#b39964'},{'lightness': 40}]},
+                                       {'featureType': 'all','elementType': 'labels.text.stroke','stylers':
+                                       [{'visibility': 'on'},{'color': '#000000'},{'lightness': 16}]},
+                                       {'featureType': 'all','elementType': 'labels.icon','stylers':
+                                       [{'visibility': 'off'}]},
+                                       {'featureType': 'administrative','elementType': 'geometry.fill','stylers':
+                                       [{'color': '#000000'},{'lightness': 20}]},
+                                       {'featureType': 'administrative','elementType': 'geometry.stroke','stylers':
+                                       [{'color': '#000000'},{'lightness': 17},{'weight': 1.2}]},
+                                       {'featureType': 'landscape','elementType': 'geometry','stylers':
+                                       [{'color': '#000000'},{'lightness': 20}]},
+                                       {'featureType': 'poi','elementType': 'geometry','stylers':
+                                       [{'color': '#000000'},{'lightness': 21}]},
+                                       {'featureType': 'road.highway','elementType': 'geometry.fill','stylers':
+                                       [{'color': '#000000'},{'lightness': 17}]},
+                                       {'featureType': 'road.highway','elementType': 'geometry.stroke','stylers':
+                                       [{'color': '#000000'},{'lightness': 29},{'weight': 0.2}]},
+                                       {'featureType': 'road.arterial','elementType': 'geometry','stylers':
+                                       [{'color': '#000000'},{'lightness': 18}]},
+                                       {'featureType': 'road.local','elementType': 'geometry','stylers':
+                                       [{'color': '#181818'},{'lightness': 16}]},
+                                       {'featureType': 'transit','elementType': 'geometry','stylers':
+                                       [{'color': '#000000'},{'lightness': 19}]},
+                                       {'featureType': 'water','elementType': 'geometry','stylers':
+                                       [{'lightness': 17},{'color': '#525252'}]}]
+                      
 				var map = new google.maps.Map(document.getElementById('map'), {
 					center: {lat: lattitude, lng: longitude},
 					zoom: zoom_level,
@@ -39,9 +65,19 @@ function initMap()
 					scaleControl: false,
 					scrollwheel: true,
 					disableDoubleClickZoom: false,
+                    streetViewControl: false,
+                    mapTypeControlOptions: {
+                                              mapTypeIds: [
+                                                           google.maps.MapTypeId.ROADMAP,
+                                                           google.maps.MapTypeId.SATELLITE,
+                                                           'dark_style',
+                                                           ]}
 					});
-		
-				var infowindow = new google.maps.InfoWindow();
+                      
+                var styledMap = new google.maps.StyledMapType(darkStyle, {name: 'Dark'});
+                map.mapTypes.set('dark_style', styledMap);
+				
+                var infowindow = new google.maps.InfoWindow();
 			
 				var marker, i;
 		
