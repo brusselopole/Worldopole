337a741408543e0649be12dcc4a7f5eba7de64c0
klatterhans <intellhosp@gmail.com>
2017-02-10 17:12:26 +0100

    Update pokemon.maps.js

--- core/js/pokemon.maps.js
+++ core/js/pokemon.maps.js
@@ -18,56 +18,6 @@ function initMap() {
 		var zoom_level = Number(variables['system']['zoom_level']);
 		var pokeimg_suffix=jsondata['system']['pokeimg_suffix'];
 
-              
-		var darkStyle = [{'featureType': 'all','elementType': 'labels.text.fill','stylers':
-                                    	[{'saturation': 36},{'color': '#b39964'},{'lightness': 40}]},
-                                    {'featureType': 'all','elementType': 'labels.text.stroke','stylers':
-                                    	[{'visibility': 'on'},{'color': '#000000'},{'lightness': 16}]},
-                                    {'featureType': 'all','elementType': 'labels.icon','stylers':
-                                        [{'visibility': 'off'}]},
-                                    {'featureType': 'administrative','elementType': 'geometry.fill','stylers':
-                                        [{'color': '#000000'},{'lightness': 20}]},
-                                    {'featureType': 'administrative','elementType': 'geometry.stroke','stylers':
-                                        [{'color': '#000000'},{'lightness': 17},{'weight': 1.2}]},
-                                    {'featureType': 'landscape','elementType': 'geometry','stylers':
-                                        [{'color': '#000000'},{'lightness': 20}]},
-                                    {'featureType': 'poi','elementType': 'geometry','stylers':
-                                        [{'color': '#000000'},{'lightness': 21}]},
-                                    {'featureType': 'road.highway','elementType': 'geometry.fill','stylers':
-                                        [{'color': '#000000'},{'lightness': 17}]},
-                                    {'featureType': 'road.highway','elementType': 'geometry.stroke','stylers':
-                                        [{'color': '#000000'},{'lightness': 29},{'weight': 0.2}]},
-                                    {'featureType': 'road.arterial','elementType': 'geometry','stylers':
-                                        [{'color': '#000000'},{'lightness': 18}]},
-                                    {'featureType': 'road.local','elementType': 'geometry','stylers':
-                                        [{'color': '#181818'},{'lightness': 16}]},
-                                    {'featureType': 'transit','elementType': 'geometry','stylers':
-                                        [{'color': '#000000'},{'lightness': 19}]},
-                                    {'featureType': 'water','elementType': 'geometry','stylers':
-                                        [{'lightness': 17},{'color': '#525252'}]}]
-					
-		var pogoStyle = [{'featureType': 'landscape.man_made','elementType': 'geometry.fill','stylers':
-                                    	[{'color': '#a1f199'}]},
-                                    {'featureType': 'landscape.natural.landcover','elementType': 'geometry.fill','stylers':
-                                    	[{'color': '#37bda2'}]},
-                                    {'featureType': 'landscape.natural.terrain','elementType': 'geometry.fill','stylers':
-                                    	[{'color': '#37bda2'}]},
-                                    {'featureType': 'poi.attraction','elementType': 'geometry.fill','stylers':
-                                    	[{'visibility': 'on'}]},
-                                    {'featureType': 'poi.business','elementType': 'geometry.fill','stylers':
-                                    	[{'color': '#e4dfd9'}]},
-                                    {'featureType': 'poi.business','elementType': 'labels.icon','stylers':
-                                    	[{'visibility': 'off'}]},
-                                    {'featureType': 'poi.park','elementType': 'geometry.fill','stylers':
-                                    	[{'color': '#37bda2'}]},
-                                    {'featureType': 'road','elementType': 'geometry.fill','stylers':
-                                    	[{'color': '#84b09e'}]},
-                                    {'featureType': 'road','elementType': 'geometry.stroke','stylers':
-                                    	[{'color': '#fafeb8'}, {'weight': '1.25'}]},
-                                    {'featureType': 'road.highway','elementType': 'labels.icon','stylers':
-                                    	[{'visibility': 'off'}]},
-                                    {'featureType': 'water','elementType': 'geometry.fill','stylers':
-                                    	[{'color': '#5ddad6'}]}]
                           
 		map = new google.maps.Map(document.getElementById('map'), {
 						center: {lat: lattitude, lng: longitude},
@@ -85,88 +35,32 @@ function initMap() {
                                 ]}
 					});
                           
+              $.get( 'core/js/pogostyle.js', function( data ) {
+                    if (data) {
+                    pogoStyle = JSON.parse(data);
+                    }
+                    
+                    var styledMap_pogo = new google.maps.StyledMapType(pogoStyle, {name: 'PoGo'});
+                    map.mapTypes.set('pogo_style', styledMap_pogo);
+                    });
+              
+              $.get( 'core/js/darkstyle.js', function( data ) {
+                    if (data) {
+                    darkStyle = JSON.parse(data);
+                    }
+                    
                     var styledMap_dark = new google.maps.StyledMapType(darkStyle, {name: 'Dark'});
                     map.mapTypes.set('dark_style', styledMap_dark);
-					
-					var styledMap_pogo = new google.maps.StyledMapType(pogoStyle, {name: 'PoGo'});
-                    map.mapTypes.set('pogo_style', styledMap_pogo);
-
-		map.set('styles',[
-			{
-				"featureType":"all",
-				"elementType":"labels.text.fill",
-				"stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]
-			},
-			{
-				"featureType":"all",
-				"elementType":"labels.text.stroke",
-				"stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]
-			},
-			{
-				"featureType":"all",
-				"elementType":"labels.icon",
-				"stylers":[{"visibility":"off"}]
-			},
-			{
-				"featureType":"administrative",
-				"elementType":"geometry.fill",
-				"stylers":[{"color":"#fefefe"},{"lightness":20}]
-			},
-			{
-				"featureType":"administrative",
-				"elementType":"geometry.stroke",
-				"stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]
-			},
-			{
-				"featureType":"landscape",
-				"elementType":"geometry",
-				"stylers":[{"color":"#f5f5f5"},{"lightness":20}]
-			},
-			{
-				"featureType":"poi",
-				"elementType":"geometry",
-				"stylers":[{"color":"#f5f5f5"},{"lightness":21}]
-			},
-			{
-				"featureType":"poi.park",
-				"elementType":"geometry",
-				"stylers":[{"color":"#dedede"},{"lightness":21}]
-			},
-			{
-				"featureType":"poi.park",
-				"elementType":"geometry.fill",
-				"stylers":[{"color":"#c2ffd7"}]
-			},
-			{
-				"featureType":"road.highway",
-				"elementType":"geometry.fill",
-				"stylers":[{"color":"#ffffff"},{"lightness":17}]},
-			{
-				"featureType":"road.highway",
-				"elementType":"geometry.stroke",
-				"stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},
-			{
-				"featureType":"road.arterial",
-				"elementType":"geometry",
-				"stylers":[{"color":"#ffffff"},{"lightness":18}]},
-			{
-				"featureType":"road.local",
-				"elementType":"geometry",
-				"stylers":[{"color":"#ffffff"},{"lightness":16}]},
-			{
-				"featureType":"transit",
-				"elementType":"geometry",
-				"stylers":[{"color":"#f2f2f2"},{"lightness":19}]},
-			{
-				"featureType":"water",
-				"elementType":"geometry",
-				"stylers":[{"color":"#e9e9e9"},{"lightness":17}]},
-			{
-				"featureType":"water",
-				"elementType":"geometry.fill",
-				"stylers":[{"color":"#b3d8f9"}]
-			}
-		]);
+                    });
+              
+              $.get( 'core/js/defaultstyle.js', function( data ) {
+                    if (data) {
+                    defaultStyle = JSON.parse(data);
+                    }
+                    
+                    map.set('styles',defaultStyle);
+                    });
+		
 		initHeatmap();
 		initSelector(pokeimg_suffix);
 		});
