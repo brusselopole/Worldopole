588f2fa9d6376ef039e6bb0a12c00b62c1a2e268
klatterhans <intellhosp@gmail.com>
2017-02-10 17:13:17 +0100

    Update gym.maps.js

--- core/js/gym.maps.js
+++ core/js/gym.maps.js
@@ -30,56 +30,6 @@ function initMap()
 					for (i = 0; i < locations.length; i++) {
 						arr.push(JSON.parse(locations[i]));
 					}
-				
-                    var darkStyle = [{'featureType': 'all','elementType': 'labels.text.fill','stylers':
-                                    	[{'saturation': 36},{'color': '#b39964'},{'lightness': 40}]},
-                                    {'featureType': 'all','elementType': 'labels.text.stroke','stylers':
-                                    	[{'visibility': 'on'},{'color': '#000000'},{'lightness': 16}]},
-                                    {'featureType': 'all','elementType': 'labels.icon','stylers':
-                                        [{'visibility': 'off'}]},
-                                    {'featureType': 'administrative','elementType': 'geometry.fill','stylers':
-                                        [{'color': '#000000'},{'lightness': 20}]},
-                                    {'featureType': 'administrative','elementType': 'geometry.stroke','stylers':
-                                        [{'color': '#000000'},{'lightness': 17},{'weight': 1.2}]},
-                                    {'featureType': 'landscape','elementType': 'geometry','stylers':
-                                        [{'color': '#000000'},{'lightness': 20}]},
-                                    {'featureType': 'poi','elementType': 'geometry','stylers':
-                                        [{'color': '#000000'},{'lightness': 21}]},
-                                    {'featureType': 'road.highway','elementType': 'geometry.fill','stylers':
-                                        [{'color': '#000000'},{'lightness': 17}]},
-                                    {'featureType': 'road.highway','elementType': 'geometry.stroke','stylers':
-                                        [{'color': '#000000'},{'lightness': 29},{'weight': 0.2}]},
-                                    {'featureType': 'road.arterial','elementType': 'geometry','stylers':
-                                        [{'color': '#000000'},{'lightness': 18}]},
-                                    {'featureType': 'road.local','elementType': 'geometry','stylers':
-                                        [{'color': '#181818'},{'lightness': 16}]},
-                                    {'featureType': 'transit','elementType': 'geometry','stylers':
-                                        [{'color': '#000000'},{'lightness': 19}]},
-                                    {'featureType': 'water','elementType': 'geometry','stylers':
-                                        [{'lightness': 17},{'color': '#525252'}]}]
-					
-					var pogoStyle = [{'featureType': 'landscape.man_made','elementType': 'geometry.fill','stylers':
-                                    	[{'color': '#a1f199'}]},
-                                    {'featureType': 'landscape.natural.landcover','elementType': 'geometry.fill','stylers':
-                                    	[{'color': '#37bda2'}]},
-                                    {'featureType': 'landscape.natural.terrain','elementType': 'geometry.fill','stylers':
-                                    	[{'color': '#37bda2'}]},
-                                    {'featureType': 'poi.attraction','elementType': 'geometry.fill','stylers':
-                                    	[{'visibility': 'on'}]},
-                                    {'featureType': 'poi.business','elementType': 'geometry.fill','stylers':
-                                    	[{'color': '#e4dfd9'}]},
-                                    {'featureType': 'poi.business','elementType': 'labels.icon','stylers':
-                                    	[{'visibility': 'off'}]},
-                                    {'featureType': 'poi.park','elementType': 'geometry.fill','stylers':
-                                    	[{'color': '#37bda2'}]},
-                                    {'featureType': 'road','elementType': 'geometry.fill','stylers':
-                                    	[{'color': '#84b09e'}]},
-                                    {'featureType': 'road','elementType': 'geometry.stroke','stylers':
-                                    	[{'color': '#fafeb8'}, {'weight': '1.25'}]},
-                                    {'featureType': 'road.highway','elementType': 'labels.icon','stylers':
-                                    	[{'visibility': 'off'}]},
-                                    {'featureType': 'water','elementType': 'geometry.fill','stylers':
-                                    	[{'color': '#5ddad6'}]}]
                           
 					var map = new google.maps.Map(document.getElementById('map'), {
 						center: {lat: lattitude, lng: longitude},
@@ -97,11 +47,31 @@ function initMap()
                                                                ]}
 					});
                           
+              $.get( 'core/js/pogostyle.js', function( data ) {
+                    if (data) {
+                    pogoStyle = JSON.parse(data);
+                    }
+                    
+                    var styledMap_pogo = new google.maps.StyledMapType(pogoStyle, {name: 'PoGo'});
+                    map.mapTypes.set('pogo_style', styledMap_pogo);
+                    });
+              
+              $.get( 'core/js/darkstyle.js', function( data ) {
+                    if (data) {
+                    darkStyle = JSON.parse(data);
+                    }
+                    
                     var styledMap_dark = new google.maps.StyledMapType(darkStyle, {name: 'Dark'});
                     map.mapTypes.set('dark_style', styledMap_dark);
-					
-					var styledMap_pogo = new google.maps.StyledMapType(pogoStyle, {name: 'PoGo'});
-                    map.mapTypes.set('pogo_style', styledMap_pogo);
+                    });
+              
+              $.get( 'core/js/defaultstyle.js', function( data ) {
+                    if (data) {
+                    defaultStyle = JSON.parse(data);
+                    }
+                    
+                    map.set('styles',defaultStyle);
+                    });
                           
 					var infowindow = new InfoBox({
 						content: document.getElementById("gym_details_template"),
