f9fb6c4b6d95dd1d9d6c7e474895e50373709096
klatterhans <intellhosp@gmail.com>
2017-02-11 11:38:29 +0100

    Forgot }

--- pages/home.page.php
+++ pages/home.page.php
@@ -100,6 +100,7 @@
 								</div>
 							</div>
 					<?php
+							   }
 					} else { ?>
 					<div class="progress" style="height: 6px; width: 80%; margin: 5px auto 15px auto;">
 						<div title="IV not available" class="progress-bar" role="progressbar" style="width: 100%; background-color: rgb(210,210,210);" aria-valuenow="1" aria-valuemin="0" aria-valuemax="1">
