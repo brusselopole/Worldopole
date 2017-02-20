a7d0e43dc04e34276336218659cf6fd73ce1a56b
Arengo <PokemonGoMap@outlook.de>
2017-02-20 23:46:02 +0100

    Add total Pokemon counter to pokedex.page.php

--- pages/pokedex.page.php
+++ pages/pokedex.page.php
@@ -4,7 +4,12 @@
 			<h1>
 				<?= $locales->POKEDEX_TITLE ?>
 			</h1>
-
+			<h3>
+				<?= sprintf($locales->POKEDEX_TOTAL, number_format($total), $config->infos->city) ?>
+			</h3>
+			/*<p>
+				<?= $locales->POKEDEX_ULTRA_COMMONS ?>
+			</p>*/
 		</div>
 	</div>
 </header>
