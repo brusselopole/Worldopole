4deffba5cc39027ed28f7cb1312794e28ed547dc
Obihörnchen <obihoernchende@gmail.com>
2017-04-06 10:33:52 +0200

    Use pokemon blacklist

--- core/cron/nests.cron.php
+++ core/cron/nests.cron.php
@@ -9,13 +9,13 @@
 
 include_once(SYS_PATH.'/core/process/locales.loader.php');
 
-$nest_pokemon_ids = implode(",", $config->system->nest_pokemon);
+$nest_exclude_pokemon_ids = implode(",", $config->system->nest_exclude_pokemon);
 
 $req = "SELECT p.pokemon_id, max(p.latitude) as latitude, max(p.longitude) as longitude, count(p.pokemon_id) as total_pokemon
 FROM pokemon p 
 INNER JOIN spawnpoint s ON (p.spawnpoint_id = s.id) 
 WHERE p.disappear_time > UTC_TIMESTAMP - INTERVAL 24 HOUR 
-AND p.pokemon_id IN (".$nest_pokemon_ids.")
+AND p.pokemon_id NOT IN (".$nest_pokemon_ids.")
 GROUP BY p.spawnpoint_id, p.pokemon_id 
 HAVING count(p.pokemon_id) >= 6 
 ORDER BY p.pokemon_id";
