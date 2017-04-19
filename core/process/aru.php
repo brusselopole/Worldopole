8e6e4857387699c59388b05f87bf75fa3361cd7d
pokemonkiel <intellhosp@gmail.com>
2017-04-19 21:14:45 +0200

    Get data from spawnpoints is faster!
    Thanks Obihörnchen

--- core/process/aru.php
+++ core/process/aru.php
@@ -710,7 +710,7 @@ switch ($request) {
 
 	case 'pokemon_coordinates_area':
 		$json="";
-		$req 		 = "SELECT MAX(latitude) AS max_latitude, MIN(latitude) AS min_latitude, MAX(longitude) AS max_longitude, MIN(longitude) as min_longitude FROM pokemon";
+		$req 		 = "SELECT MAX(latitude) AS max_latitude, MIN(latitude) AS min_latitude, MAX(longitude) AS max_longitude, MIN(longitude) as min_longitude FROM spawnpoint";
 		$result 	 = $mysqli->query($req);
 		$data 		 = $result->fetch_object();
 		$coordinates = $data;
