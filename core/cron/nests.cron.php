0559328f6dfc24d95f897992f0367905cc430580
Markus Hilger <obihoernchende@gmail.com>
2017-04-21 19:34:26 +0200

    Consistency

--- core/cron/nests.cron.php
+++ core/cron/nests.cron.php
@@ -11,7 +11,7 @@ $nest_exclude_pokemon_ids = implode(",", $config->system->nest_exclude_pokemon);
 $req = "SELECT p.pokemon_id, max(p.latitude) as latitude, max(p.longitude) as longitude, count(p.pokemon_id) as total_pokemon
 FROM pokemon p 
 INNER JOIN spawnpoint s ON (p.spawnpoint_id = s.id) 
-WHERE p.disappear_time > UTC_TIMESTAMP - INTERVAL 24 HOUR 
+WHERE p.disappear_time > UTC_TIMESTAMP() - INTERVAL 24 HOUR
 AND p.pokemon_id NOT IN (".$nest_exclude_pokemon_ids.")
 GROUP BY p.spawnpoint_id, p.pokemon_id 
 HAVING count(p.pokemon_id) >= 6 
