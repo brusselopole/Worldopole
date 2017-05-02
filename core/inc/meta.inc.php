f2de8dcb65da65a819b52004cd8efb570ec41278
Michael Krug <krumi@hrz.tu-chemnitz.de>
2017-05-02 11:29:00 +0200

    remove space in pokemon title fix meta urls

--- core/inc/meta.inc.php
+++ core/inc/meta.inc.php
@@ -30,7 +30,7 @@
 		<meta property="og:title" content="<?= $config->infos->site_name ?> | <?= strip_tags($config->infos->site_title); ?> " />
 		<meta property="og:description" content="<?= sprintf($locales->METADESCRIPTION, $config->infos->city); ?>" />
 
-		<meta property="og:url" content="<?= HOST_URL ?>/" />
+		<meta property="og:url" content="<?= HOST_URL ?>" />
 		<meta property="og:site_name" content="<?= $config->infos->site_name ?>" />
 		<meta property="og:image" content="<?= HOST_URL ?>core/img/logo.jpg" />
 
@@ -48,7 +48,7 @@
 		case 'pokemon':
 		?>
 		
-		<title><?= $config->infos->site_name ?> | <?= $locales->FIND ?> <?= $pokemon->name ?> <?= sprintf($locales->INCITY, $config->infos->city); ?></title>
+		<title><?= $config->infos->site_name ?> | <?= $locales->FIND ?><?= $pokemon->name ?> <?= sprintf($locales->INCITY, $config->infos->city); ?></title>
 
 		
 		<link rel="canonical" href="<?= HOST_URL ?>pokemon/<?= $pokemon->id ?>" />
@@ -57,13 +57,13 @@
 		<meta property="og:locale" content="en_US" />
 		<meta property="og:type" content="website" />
 		<meta property="og:title" content="<?= $config->infos->site_name ?> | <?= $pokemon->name ?>" />
-		<meta property="og:description" content="<?= $locales->FIND ?> <?= $pokemon->name ?> <?= sprintf($locales->INCITY, $config->infos->city); ?>" />
-		<meta property="og:url" content="<?= HOST_URL ?>/pokemon/<?= $pokemon->id ?>" />
+		<meta property="og:description" content="<?= $locales->FIND ?><?= $pokemon->name ?> <?= sprintf($locales->INCITY, $config->infos->city); ?>" />
+		<meta property="og:url" content="<?= HOST_URL ?>pokemon/<?= $pokemon->id ?>" />
 		<meta property="og:site_name" content="<?= $config->infos->site_name ?>" />
 		<meta property="og:image" content="<?= HOST_URL ?>core/pokemons/<?= $pokemon->id ?>.png" />
 		
 		<meta name="twitter:card" content="summary" />
-		<meta name="twitter:description" content="<?= $locales->FIND ?> <?= $pokemon->name ?> <?= sprintf($locales->INCITY, $config->infos->city); ?>" />
+		<meta name="twitter:description" content="<?= $locales->FIND ?><?= $pokemon->name ?> <?= sprintf($locales->INCITY, $config->infos->city); ?>" />
 		<meta name="twitter:title" content="<?= $config->infos->site_name ?> | <?= $pokemon->name ?>" />
 		<meta name="twitter:image" content="<?= HOST_URL ?>core/pokemons/<?= $pokemon->id ?>.png" />
 		
@@ -77,14 +77,14 @@
 
 		<title><?= $config->infos->site_name ?> | <?= sprintf($locales->POKESTOPS_METATITLE, $config->infos->city); ?></title>
 		
-		<link rel="canonical" href="<?= HOST_URL ?>/pokestops" />
+		<link rel="canonical" href="<?= HOST_URL ?>pokestops" />
 		<base href="<?= HOST_URL ?>">
 
 		<meta property="og:locale" content="en_US" />
 		<meta property="og:type" content="website" />
 		<meta property="og:title" content="<?= $config->infos->site_name ?> | <?= sprintf($locales->POKESTOPS_METATITLE, $config->infos->city); ?>" />
 		<meta property="og:description" content="<?= sprintf($locales->POKESTOPS_METADESC, $config->infos->city); ?>" />
-		<meta property="og:url" content="<?= HOST_URL ?>/pokestops" />
+		<meta property="og:url" content="<?= HOST_URL ?>pokestops" />
 		<meta property="og:site_name" content="<?= $config->infos->site_name ?>" />
 		<meta property="og:image" content="<?= HOST_URL ?>core/img/logo.jpg" />
 		
@@ -102,7 +102,7 @@
 		
 		<title><?= $config->infos->site_name ?> | <?= $locales->GYMS_METATITLE ?></title>
 		
-		<link rel="canonical" href="<?= HOST_URL ?>/gym" />
+		<link rel="canonical" href="<?= HOST_URL ?>gym" />
 		<base href="<?= HOST_URL ?>">
 		
 
@@ -110,7 +110,7 @@
 		<meta property="og:type" content="website" />
 		<meta property="og:title" content="<?= $config->infos->site_name ?> | <?= $locales->GYMS_METATITLE ?>" />
 		<meta property="og:description" content="<?= $locales->GYMS_METADESC ?>" />
-		<meta property="og:url" content="<?= HOST_URL ?>/gym" />
+		<meta property="og:url" content="<?= HOST_URL ?>gym" />
 		<meta property="og:site_name" content="<?= $config->infos->site_name ?>" />
 		<meta property="og:image" content="<?= HOST_URL ?>core/img/logo.jpg" />
 		
@@ -127,14 +127,14 @@
 
 		<title><?= $config->infos->site_name ?> | <?= $locales->TRAINERS_METATITLE ?></title>
 
-		<link rel="canonical" href="<?= HOST_URL ?>/trainer" />
+		<link rel="canonical" href="<?= HOST_URL ?>trainer" />
 		<base href="<?= HOST_URL ?>">
 
 		<meta property="og:locale" content="en_US" />
 		<meta property="og:type" content="website" />
 		<meta property="og:title" content="<?= $config->infos->site_name ?> | <?= $locales->TRAINERS_METATITLE ?>" />
 		<meta property="og:description" content="<?= $locales->TRAINERS_METADESC ?>" />
-		<meta property="og:url" content="<?= HOST_URL ?>/trainer" />
+		<meta property="og:url" content="<?= HOST_URL ?>trainer" />
 		<meta property="og:site_name" content="<?= $config->infos->site_name ?>" />
 		<meta property="og:image" content="<?= HOST_URL ?>core/img/logo.jpg" />
 
@@ -151,14 +151,14 @@
 		
 		<title><?= $config->infos->site_name ?> | <?= $locales->NAV_DASHBOARD ?></title>
 		
-		<link rel="canonical" href="<?= HOST_URL ?>/dashboard" />
+		<link rel="canonical" href="<?= HOST_URL ?>dashboard" />
 		<base href="<?= HOST_URL ?>">
 
 		<meta property="og:locale" content="en_US" />
 		<meta property="og:type" content="website" />
 		<meta property="og:title" content="<?= $config->infos->site_name ?> | <?= $locales->NAV_DASHBOARD ?>" />
 		<meta property="og:description" content="Data lover? We've got plenty for you!" />
-		<meta property="og:url" content="<?= HOST_URL ?>/dashboard" />
+		<meta property="og:url" content="<?= HOST_URL ?>dashboard" />
 		<meta property="og:site_name" content="<?= $config->infos->site_name ?>" />
 		<meta property="og:image" content="<?= HOST_URL ?>core/img/logo.jpg" />
 		
@@ -176,14 +176,14 @@
 		
 		<title><?= $config->infos->site_name ?> | <?= $locales->POKEDEX_METATITLE ?></title>
 		
-		<link rel="canonical" href="<?= HOST_URL ?>/pokemon" />
+		<link rel="canonical" href="<?= HOST_URL ?>pokemon" />
 		<base href="<?= HOST_URL ?>">
 
 		<meta property="og:locale" content="en_US" />
 		<meta property="og:type" content="website" />
 		<meta property="og:title" content="<?= $config->infos->site_name ?> | <?= $locales->POKEDEX_METATITLE ?>" />
 		<meta property="og:description" content="<?= $locales->POKEDEX_METADESC ?>" />
-		<meta property="og:url" content="<?= HOST_URL ?>/pokemon" />
+		<meta property="og:url" content="<?= HOST_URL ?>pokemon" />
 		<meta property="og:site_name" content="<?= $config->infos->site_name ?>" />
 		<meta property="og:image" content="<?= HOST_URL ?>core/img/logo.jpg" />
 		
@@ -200,14 +200,14 @@
 		
 		<title><?= $config->infos->site_name ?> | <?= $locales->NAV_NESTS ?></title>
 		
-		<link rel="canonical" href="<?= HOST_URL ?>/pokemon" />
+		<link rel="canonical" href="<?= HOST_URL ?>nests" />
 		<base href="<?= HOST_URL ?>">
 
 		<meta property="og:locale" content="en_US" />
 		<meta property="og:type" content="website" />
 		<meta property="og:title" content="<?= $config->infos->site_name ?> | <?= $locales->NAV_NESTS ?>" />
 		<meta property="og:description" content="<?= $locales->NESTS_METADESC ?>" />
-		<meta property="og:url" content="<?= HOST_URL ?>/pokemon" />
+		<meta property="og:url" content="<?= HOST_URL ?>nests" />
 		<meta property="og:site_name" content="<?= $config->infos->site_name ?>" />
 		<meta property="og:image" content="<?= HOST_URL ?>core/img/logo.jpg" />
 		
