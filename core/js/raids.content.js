0ad242fa12587a65175ff3ccf9b7ce8f43b7c2e1
Michael Krug <krumi@hrz.tu-chemnitz.de>
2017-07-10 11:24:19 +0200

    fix counter showing NaN in Safari

--- core/js/raids.content.js
+++ core/js/raids.content.js
@@ -41,8 +41,8 @@ function loadRaids(page, pokeimg_suffix) {
 
 function printRaid(raid, pokeimg_suffix) {
 	var now = new Date();
-	var raidStart = new Date(raid.start);
-	var raidEnd = new Date(raid.end);
+	var raidStart = new Date(raid.start.replace(/-/g, '/'));
+	var raidEnd = new Date(raid.end.replace(/-/g, '/'));
 
 	var raidInfos = $('<tr>',{id: 'raidInfos_'+raid.gym_id}).css('border-bottom','2px solid '+(raid.level>2?'#fad94c':'#e872b7'));
 	raidInfos.append($('<td>',{id: 'raidLevel_'+raid.gym_id, text: '★'.repeat(raid.level)}));
