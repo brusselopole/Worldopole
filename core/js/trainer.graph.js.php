bfe3b47fc8e9ad955e33cff77a14e04b9864f04e
Michael Krug <michi.krug@gmail.com>
2017-09-07 00:22:13 +0200

    apply blacklist to trainer graph

--- core/js/trainer.graph.js.php
+++ core/js/trainer.graph.js.php
@@ -29,7 +29,11 @@ if ($mysqli->connect_error != '') {
 $trainer_lvl = [];
 # For all 3 teams
 for ($teamid = 1; $teamid <= 3; $teamid++) {
-	$req = "SELECT level, count(level) AS count FROM trainer WHERE team = '".$teamid."' GROUP BY level";
+	$req = "SELECT level, count(level) AS count FROM trainer WHERE team = '".$teamid."'";
+	if (!empty($config->system->trainer_blacklist)) {
+		$req .= " AND name NOT IN ('".implode("','", $config->system->trainer_blacklist)."')";
+	}
+	$req .= " GROUP BY level";
 	if ($result = $mysqli->query($req)) {
 		# build level=>count array
 		$data = [];
