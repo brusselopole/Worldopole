70a26778f8c856f5d8cb0870a236daca5b978243
Florian Kostenzer <florian.kostenzer@icloud.com>
2018-02-01 20:27:01 +0100

    use spawn instead of despawn time

--- core/cron/pokedex_counts.cron.php
+++ core/cron/pokedex_counts.cron.php
@@ -25,9 +25,19 @@ for ($pid = 1; $pid <= $maxpid; $pid++) {
 
 	$last_update = $newpokecountdatas[$pid]['last_update'];
 
-	$req = "SELECT COUNT(*) as count, UNIX_TIMESTAMP(MAX(disappear_time)) as last_timestamp, (CONVERT_TZ(MAX(disappear_time), '+00:00', '".$time_offset."')) AS disappear_time_real, latitude, longitude 
-				FROM pokemon 
-				WHERE pokemon_id = '".$pid."' && UNIX_TIMESTAMP(disappear_time) > '".$last_update."'";
+	$where = "WHERE p.pokemon_id = '".$pokemon_id."' AND (UNIX_TIMESTAMP(p.disappear_time) - (LENGTH(s.kind) - LENGTH( REPLACE ( kind, \"s\", \"\") )) * 900) > '".$last_update."'";
+	$req = "SELECT count, (UNIX_TIMESTAMP(p.disappear_time) - (LENGTH(s.kind) - LENGTH( REPLACE ( kind, \"s\", \"\") )) * 900) as last_timestamp, (CONVERT_TZ(p.disappear_time, '+00:00', '".self::$time_offset."')) AS disappear_time_real, p.latitude, p.longitude 
+		FROM pokemon p
+		JOIN spawnpoint s ON p.spawnpoint_id = s.id
+		JOIN (
+			SELECT count(*) as count
+			FROM pokemon p
+			JOIN spawnpoint s ON p.spawnpoint_id = s.id
+			" . $where."
+		) x
+		" . $where . "
+		ORDER BY last_timestamp DESC
+		LIMIT 0,1";
 	$result = $mysqli->query($req);
 	$data = $result->fetch_object();
 
